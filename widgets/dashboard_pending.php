ff9c4a70815b98d81a51b9766d443bb676d68c6c
Gabriel Gil <gabriel@gabrielgil.es>
2015-01-30 09:16:37 +0000

    Prevents using PHP shortags

--- widgets/dashboard_pending.php
+++ widgets/dashboard_pending.php
@@ -1,4 +1,4 @@
-<?
+<?php
 
 /*
  * Widget content
@@ -10,25 +10,25 @@ function bitbucket_pending_issues_content() {
 
 	// Fetch non resolved issues
 	$issues = Bitbucket_Issue::get_issues( 'new' );
-	
+
 	//echo '<pre>'.print_r($issues, true) . '</pre>';
-	
+
 	// Display errors
     if ( Bitbucket::echo_errors( $issues ) == 1 ) {
 	    return;
     }
-    
-    
+
+
     if ( count($issues->issues) < 1 )
     {
 	    echo '<h4>' . __( 'No pending issues.', 'bim') . '</h4><p>' . __( 'Nothing to do! Add issues on your Bitbucket repository page.', 'bim' ) . '</p>';
 		return;
-    
+
     }
-    
+
 	// List issues
 	Bitbucket_Issue::print_dashboard_issue_listing( $issues );
-		
+
 	echo '<a target="_blank" style="" class="button-secondary" href="http://bitbucket.com/' . BITBUCKET_USERNAME . '/' . BITBUCKET_REPOSITORY .'/issues?status=!resolved">' . __( 'View all on Bitbucket\'s site', 'bim' ) .'</a>';
 
 }
--- widgets/dashboard_resolved.php
+++ widgets/dashboard_resolved.php
@@ -1,4 +1,4 @@
-<?
+<?php
 
 /*
  * Widget content
@@ -8,27 +8,27 @@
 
 function bitbucket_resolved_issues_content()
 {
-	
+
     // Fetch non resolved issues
 	$issues = Bitbucket_Issue::get_issues( 'resolved' );
-	
+
 	//echo '<pre>'.print_r($issues, true) . '</pre>';
-	
+
 	// Display errors
     if ( Bitbucket::echo_errors( $issues ) == 1 ) {
 	    return;
     }
-    
-    
+
+
     if ( count($issues->issues) < 1 )
     {
 	    echo '<h4>' . __( 'No solved issues.', 'bim') . '</h4><p>' . __( 'You didn\'t solved anything! Resolve issues on your Bitbucket repository page.', 'bim' ) . '</p>';
 		return;
-    
+
     }
-    
+
 	// List issues
 	Bitbucket_Issue::print_dashboard_issue_listing( $issues );
-		
+
 	echo '<a target="_blank" style="" class="button-secondary" href="http://bitbucket.com/' . BITBUCKET_USERNAME . '/' . BITBUCKET_REPOSITORY .'/issues?status=resolved">' . __( 'View all on Bitbucket\'s site', 'bim' ) .'</a>';
 }
